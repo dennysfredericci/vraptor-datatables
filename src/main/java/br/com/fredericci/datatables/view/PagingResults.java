c43b96d762bda8157c88c47a70630dffb8e504ee
Dennys <dennys@fredericci.com.br>
2012-07-14 18:04:03 -0300

    fix serialization

--- src/main/java/br/com/fredericci/datatables/view/PagingResults.java
+++ src/main/java/br/com/fredericci/datatables/view/PagingResults.java
@@ -2,7 +2,9 @@ package br.com.fredericci.datatables.view;
 
 import static br.com.caelum.vraptor.view.Results.representation;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.http.FormatResolver;
 import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.serialization.HTMLSerialization;
 import br.com.fredericci.datatables.Page;
 import br.com.fredericci.datatables.converter.DataTable;
 
@@ -10,15 +12,25 @@ import br.com.fredericci.datatables.converter.DataTable;
 public class PagingResults implements PagingResult {
 
 	private Result result;
+	private FormatResolver formatResolver;
+	private HTMLSerialization html;
 
-	public PagingResults(Result result) {
+	public PagingResults(Result result, FormatResolver formatResolver, HTMLSerialization html) {
 		this.result = result;
+		this.formatResolver = formatResolver;
+		this.html = html;
 	}
 
 	@Override
 	public void from(Page<?> page) {
-		DataTable dataTable = new DataTable(page);
-		this.result.use(representation()).from(dataTable).include("aaData").recursive().serialize();
+
+		if (html.accepts(formatResolver.getAcceptFormat())) {
+			html.from(page);
+		} else {
+			DataTable dataTable = new DataTable(page);
+			this.result.use(representation()).from(dataTable).include("aaData").serialize();
+		}
+
 	}
 
 	public static Class<PagingResults> dataTablesPaging() {
